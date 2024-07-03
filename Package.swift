8a3bb05d16e290cd076f905b9838390d99617395
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-07-03 11:31:38 -0400

    Update package def

--- Package.swift
+++ Package.swift
@@ -1,24 +1,38 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.10
 
 import PackageDescription
 
-let settings: [SwiftSetting] = [
-	.enableExperimentalFeature("StrictConcurrency")
-]
-
 let package = Package(
 	name: "OAuthenticator",
-	platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS("6.2")],
+	platforms: [
+		.macOS(.v10_15),
+		.macCatalyst(.v13),
+		.iOS(.v13),
+		.tvOS(.v13),
+		.watchOS(.v7),
+		.visionOS(.v1),
+	],
 	products: [
 		.library(name: "OAuthenticator", targets: ["OAuthenticator"]),
 	],
 	dependencies: [
 	],
 	targets: [
-		.target(name: "OAuthenticator",
-				dependencies: [],
-				resources: [.process("PrivacyInfo.xcprivacy")],
-				swiftSettings: settings),
-		.testTarget(name: "OAuthenticatorTests", dependencies: ["OAuthenticator"], swiftSettings: settings),
+		.target(
+			name: "OAuthenticator",
+			dependencies: [],
+			resources: [.process("PrivacyInfo.xcprivacy")]
+		),
+		.testTarget(name: "OAuthenticatorTests", dependencies: ["OAuthenticator"]),
 	]
 )
+
+let swiftSettings: [SwiftSetting] = [
+	.enableExperimentalFeature("StrictConcurrency"),
+]
+
+for target in package.targets {
+	var settings = target.swiftSettings ?? []
+	settings.append(contentsOf: swiftSettings)
+	target.swiftSettings = settings
+}
