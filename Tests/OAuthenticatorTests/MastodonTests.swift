271056fbd76d8002576feb406c33f059e98b81f9
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-12-27 07:03:11 -0500

    Convert MastodonTests to swift testing

--- Tests/OAuthenticatorTests/MastodonTests.swift
+++ Tests/OAuthenticatorTests/MastodonTests.swift
@@ -1,18 +1,21 @@
-import XCTest
+import Foundation
+import Testing
+
 @testable import OAuthenticator
 
-final class MastodonTests: XCTestCase {
-	func testAppResponseDecode() throws {
+struct MastodonTests {
+	@Test
+	func appResponseDecode() throws {
 		// From https://docs.joinmastodon.org/entities/Token/
 		let content = """
 {"access_token": "ZA-Yj3aBD8U8Cm7lKUp-lm9O9BmDgdhHzDeqsY8tlL0", "token_type": "bearer", "scope": "read write follow push", "created_at": 1573979017}
 """
-		let data = try XCTUnwrap(content.data(using: .utf8))
+		let data = try #require(content.data(using: .utf8))
 		let response = try JSONDecoder().decode(Mastodon.AppAuthResponse.self, from: data)
 
-		XCTAssertEqual(response.accessToken, "ZA-Yj3aBD8U8Cm7lKUp-lm9O9BmDgdhHzDeqsY8tlL0")
+		#expect(response.accessToken == "ZA-Yj3aBD8U8Cm7lKUp-lm9O9BmDgdhHzDeqsY8tlL0")
 
 		let login = response.login
-		XCTAssertEqual(login.accessToken.value, "ZA-Yj3aBD8U8Cm7lKUp-lm9O9BmDgdhHzDeqsY8tlL0")
+		#expect(login.accessToken.value == "ZA-Yj3aBD8U8Cm7lKUp-lm9O9BmDgdhHzDeqsY8tlL0")
 	}
 }
